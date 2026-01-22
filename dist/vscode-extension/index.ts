2ce9d323fedab5eea904da206ebfddf7905cf79d
Plushveil <plushveil@gray.blue>
2026-01-22 17:44:54 +0100

    style: format code for consistency and readability in index.ts

--- dist/vscode-extension/index.ts
+++ dist/vscode-extension/index.ts
@@ -3,16 +3,16 @@ import * as vscode from 'vscode'
 
 
 export function activate(context: vscode.ExtensionContext) {
-	const didChangeEmitter = new vscode.EventEmitter<void>();
-
+  const didChangeEmitter = new vscode.EventEmitter<void>();
+ 
   context.subscriptions.push(vscode.lm.registerMcpServerDefinitionProvider('tools', {
-		onDidChangeMcpServerDefinitions: didChangeEmitter.event,
-		provideMcpServerDefinitions: async () => {
+    onDidChangeMcpServerDefinitions: didChangeEmitter.event,
+    provideMcpServerDefinitions: async () => {
       const env = Object.fromEntries(Object.entries(process.env).filter(([_, v]) => v !== undefined)) as Record<string, string | number>
       const output: vscode.McpServerDefinition[] = [
         new vscode.McpStdioServerDefinition('tools', 'node', ['./mcp/mcp-tools.js'], env)
       ];
-			return output;
-		}
-	}));
+      return output;
+    }
+  }));
 }
