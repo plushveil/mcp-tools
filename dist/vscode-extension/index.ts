9893896125c898ed61d55b5bcd6a816c3375d72f
Plushveil <plushveil@gray.blue>
2026-01-23 23:20:49 +0100

    feat: update MCP server definition path and increment version to 0.0.25 in package.json

--- dist/vscode-extension/index.ts
+++ dist/vscode-extension/index.ts
@@ -22,7 +22,7 @@ export function activate (context: vscode.ExtensionContext) {
     provideMcpServerDefinitions: () => {
       const env = Object.fromEntries(Object.entries(process.env).filter(([_, v]) => v !== undefined)) as Record<string, string | number>
       const output: vscode.McpServerDefinition[] = [
-        new vscode.McpStdioServerDefinition('plushveil.mcp-tools', node, [path.resolve(__dirname, 'mcp', 'mcp-tools.js')], env)
+        new vscode.McpStdioServerDefinition('plushveil.mcp-tools', node, [path.resolve(__dirname, 'mcp.js')], env)
       ]
       return output
     },
