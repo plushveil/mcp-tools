52247b2e7a57a4f967f9441ec92ecb6ff8f3f3fa
Plushveil <plushveil@gray.blue>
2026-01-23 23:46:46 +0100

    feat: update MCP server definition provider and increment version to 0.0.26 in package.json

--- dist/vscode-extension/index.ts
+++ dist/vscode-extension/index.ts
@@ -17,10 +17,10 @@ export function activate (context: vscode.ExtensionContext) {
   if (!node) throw new Error('process.execPath is undefined')
 
   const didChangeEmitter = new vscode.EventEmitter<void>()
-  const mcp = vscode.lm.registerMcpServerDefinitionProvider('plushveil.mcp-tools', {
+  const mcpServerDefinitionProvider : vscode.McpServerDefinitionProvider = {
     onDidChangeMcpServerDefinitions: didChangeEmitter.event,
     provideMcpServerDefinitions: () => {
-      const env = Object.fromEntries(Object.entries(process.env).filter(([_, v]) => v !== undefined)) as Record<string, string | number>
+      const env = process.env as Record<string, string>
       const output: vscode.McpServerDefinition[] = [
         new vscode.McpStdioServerDefinition('plushveil.mcp-tools', node, [path.resolve(__dirname, 'mcp.js')], env)
       ]
@@ -29,8 +29,8 @@ export function activate (context: vscode.ExtensionContext) {
     resolveMcpServerDefinition: async (definition: vscode.McpServerDefinition) => {
       return definition
     }
-  })
-  context.subscriptions.push(mcp)
+  }
+  context.subscriptions.push(vscode.lm.registerMcpServerDefinitionProvider('plushveil.mcp-tools', mcpServerDefinitionProvider))
 }
 
 /**
